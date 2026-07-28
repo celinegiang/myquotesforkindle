fd6ad97faa66996d4471c975b413dda701884532
Celine <68299431+celinegiang@users.noreply.github.com>
2026-07-28 07:01:48 +0000

    fuck i added anime

---
+++ my_quotes.lua
@@ -0,0 +1,113 @@
+return {
+    {
+        q = "I am constantly trying to communicate something incommunicable, to explain something inexplicable.",
+        a = "Franz Kafka"
+    },
+
+    {
+        q = "You are the knife I turn inside myself; that is love.",
+        a = "Franz Kafka",
+        b = "Letters to Milena"
+    },
+
+    {
+        q = "Because if you walk into a room and notice what is missing from it, It's still there, isn't it? The first poem I wrote that wasn't about you was still about you",
+        a = "Caitlyn Siehl",
+        b = "What We Buried; A Letter To Love"
+    },
+
+    {
+        q = "I miss you more than I remember you.",
+        a = "Ocean Vuong",
+        b = "On Earth, We're Briefly Gorgeous"
+    },
+
+    {
+        q = "Where you used to be, there is a hole in the world, which I find myself constantly walking around in the daytime, and falling into at night.",
+        a = "Edna St. Vincent Millay"
+    },
+
+    {
+        q = "In another life, I think I would have really liked just doing laundry and taxes with you.",
+        a = "Waymond Wang",
+        b = "Everything Everywhere All at Once"
+    },
+
+    {
+        q = "Whatever our souls are made of, his and mine are the same.",
+        a = "Emily Brontë",
+        b = "Wuthering Heights"
+    },
+
+    {
+        q = "But you didn't learn did you? Which makes all the sense in the world because.. I still dream of Krypton.",
+        a = "Kara Zor'el",
+        b = "Supergirl: Woman of Tomorrow"
+    },
+
+    {
+        q = "You wouldn't believe the kind of person I could become if you wanted it.",
+        a = "Franz Kafka",
+        b = "Letters to Felice"
+    },
+
+    {
+        q = "There are times when my longing for you overwhelms me [...] so often I can think of you only with teeth clenched.",
+        a = "Franz Kafka",
+        b = "Letters to Felice"
+    },
+
+    {
+        q = "If I loved you less, I might be able to talk about it more.",
+        a = "Jane Austen",
+        b = "Emma"
+    },
+
+    {
+        q = "I think I've loved you since I met you. I just mistook it for curiosity.",
+        a = "Alice Oseman",
+        b = "Solitaire"
+    },
+
+    {
+        q = "In case you ever foolishly forget: I am never not thinking of you.",
+        a = "Virginia Woolf"
+    },
+
+    {
+        q = "I just miss you, in a quite, simple, desperate, human way.",
+        a = "Vita Sackville-West",
+        b = "Excerpt from a letter to Virginia Woolf"
+    },
+
+    {
+        q = "In the dream I don't tell anyone, you put your head in my lap.",
+        a = "Richard Siken"
+    },
+
+    {
+        q = "What, like it's hard?",
+        a = "Elle Woods",
+        b = "Legally Blonde"
+    },
+
+    {
+        q = "From the moment we kissed on the beach, I thought you knew!",
+        a = "Conrad Fisher",
+        b = "The Summer I Turned Pretty"
+    },
+
+    {
+        q = "I still love you. I don't think I'll ever get you out of my system.",
+        a = "Conrad Fisher",
+        b = "The Summer I Turned Pretty"
+    },
+
+    {
+        q = "I've never admit something like this to your face, But the truth is, without meaning to, I start talking like you so mean sometimes, and I hate it. But my idea of victory is so tied to the image of you in my head that in those moments, I can't help but imitate you.",
+        a = "Izuku Midoriya",
+        b = "My Hero Academia"
+    }
+
+
+}
